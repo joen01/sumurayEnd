f50050c950567535d59f9e721b67b2d373e2fa83
Evgeny012 <joeyskirilov@gmail.com>
2026-05-05 12:07:55 +0300

    fix title header

--- src/iu/Header.tsx
+++ src/iu/Header.tsx
@@ -2,7 +2,7 @@ import f from '../App.module.css'
 const Header = () => {
     return (
         <header className={f.header}>
-            <h1> Musicfun Player </h1>
+            <div> Musicfun Player </div>
             <img alt="avatar" src='https://avatars.mds.yandex.net/i?id=f73b461568aba1ae27b2a204795c9407e4f65df9-4820979-images-thumbs&n=13'/>
         </header>
     )
